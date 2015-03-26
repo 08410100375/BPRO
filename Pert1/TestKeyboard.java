4feb051ca3ec7537771dca59594509ca58a27856
teguhsutanto <teguh.sutanto@gmail.com>
2015-03-26 13:27:50 +0700

    Pertemuan 1

---
+++ Pert1/TestKeyboard.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TestKeyboard{
+  public static void main(String []argv){
+  
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Input kata : "); 
+    String kata=sc.next();
+    System.out.print("Input int : "); 
+    int bil=sc.nextInt();
+  }
+
+}
